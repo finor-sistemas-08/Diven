2e5080ad44de6a092c1feaed3c18fa22cd5b8462
Paul <jeanpaulflores2014@gmail.com>
2021-01-23 18:01:06 -0400

    El login diseñado y listo, solo falta programarlo

--- lib/main.dart
+++ lib/main.dart
@@ -5,109 +5,86 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'Material App',
+      home: Scaffold(        
+        //El diseño del Login se carga de esta manera
+        body: cuerpo(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+//Widget con la imagen de fondo y el cuerpo del Login
+Widget cuerpo() {
+  return Container(
+    decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage("https://wallpapercave.com/wp/wp2873698.png"),
+            fit: BoxFit.cover)),
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          nombre(),
+          SizedBox(height: 7),
+          campoUsuario(),
+          campoContrasena(),
+          SizedBox(height: 7),
+          botonEntrar(),
+        ],
+      ),
+    ),
+  );
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+//Widget para el título de "Iniciar Sesión"
+Widget nombre() {
+  return Text(
+    "Inicie Sesión",
+    style: TextStyle(
+        color: Colors.cyan[50], 
+        fontSize: 31.0, 
+        fontWeight: FontWeight.bold
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Por fin esto funciona demonios',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+  );
+}
+
+//Campo para insertar datos de usuario
+Widget campoUsuario(){
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 3),
+    child: TextField(
+      decoration: InputDecoration(
+        hintText: "Usuario",
+        fillColor: Colors.white,
+        filled: true,      
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    ),
+  );
+}
+
+Widget campoContrasena(){
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 3),
+    child: TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Contraseña",
+        fillColor: Colors.white,
+        filled: true,      
+      ),
+    ),
+  );
+}
+
+Widget botonEntrar(){
+  return FlatButton(
+    color: Colors.cyan[900],
+    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+    onPressed: (){},
+    child: Text("Entrar", style: TextStyle(fontSize: 18, color: Colors.white))
+  );
 }
