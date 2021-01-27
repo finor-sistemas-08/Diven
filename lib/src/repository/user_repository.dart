af682d677a1b44a76a83099e652b24a99ff5ad47
Paul <jeanpaulflores2014@gmail.com>
2021-01-27 00:16:24 -0400

    Creado repository.dart y agregado a pubspec.yaml los plugins necesarios, un pequeño cambio en pubspec.lock también

---
+++ lib/src/repository/user_repository.dart
@@ -0,0 +1,21 @@
+//Imports
+// import 'dart:async';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+
+// class UserRepository{
+//   final FirebaseAuth _firebaseAuth;
+//   final GoogleSignIn _googleSignIn;
+// }
+
+//Constructor
+// UserRepository({FirebaseAuth firebaseAuth, GoogleSignIn googleSignIn})
+//   : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+//   _googleSignIn = googleSignIn ?? GoogleSignIn();
+
+//SignInWithGoogle
+//SignInWithCredencials
+//SignUp - Registro
+//SignOut
+//Está logeado?
+//Obtener usuario
